66451bfb52abbd8188d805cc93bea89a8e6b20a3
Matthew Wang <mattwyl@gmail.com>
2012-11-08 11:27:51 +0800

    simplify, almost no extra space

--- py/equi.py
+++ py/equi.py
@@ -1,23 +1,20 @@
 #!/usr/bin/env python
-# http://codility.com/demo/results/demoVRPKX7-TSH/
+# http://codility.com/demo/results/demoAVPM9H-FYB/
 
 def equi(A):
-    # Temp array to store sum value for each index, where position i stores sum
-    # of A[0..i-1], like this:
-    #
-    # [0, sum for [0], sum for [1], ..., sum for [n-1], 0]
-    #
-    sum_index = [0]
+    # Temp counter to store sum of left part and right part accordingly
+    left = 0
+    right = sum(A)
     for i in xrange(len(A)):
-        sum_index.append(sum_index[i] + A[i])
-    sum_index.append(0)
-
-    for i in xrange(len(A)):
-        if sum_index[i] == sum_index[-2] - A[i] - sum_index[i]:
+        right -= A[i]
+        if left == right:
             return i
+        else:
+            left += A[i]
     return -1
 
 if __name__ == '__main__':
     print equi([-7, 1, 5, 2, -4, 3, 0])
-    print equi([-1, 0, 1, -1, 0, 1])
+    print equi([-1, 0, 1, -1, 0, 1, 6])
     print equi([0])
+    print equi([])
