1427ac49eeb2204714ee8dbbc7acffc37edecf81
Matthew Wang <mattwyl@gmail.com>
2013-01-24 18:24:24 +0800

    echo -e "\e" does not work for bash on mac

--- sh/ansicolor.sh
+++ sh/ansicolor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 for x in {30..37}; do
-    echo -e "\e[${x}m $x Hello world! \e[0m"
-    echo -e "\e[01;${x}m 01;$x Hello world! \e[0m"
+    echo -e "\x1b[${x}m $x Hello world! \x1b[0m"
+    echo -e "\x1b[01;${x}m 01;$x Hello world! \x1b[0m"
 done
